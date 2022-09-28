79a0f34134e1910a66139b9a0e7d3bf3d54ed817
zach vallarino <zvcoding@gmail.com>
2022-09-27 20:16:53 -0400

    basically complete check the input when trends are up on mobile

--- components/Header.js
+++ components/Header.js
@@ -5,18 +5,18 @@ function Header() {
 
   return (
     <>
-      <div className='border border-popBlue border-4 col-span-2 row-span-1 mx-20 mt-2'>
-            <div className='flex justify-center bg-white '>
+      <div className='hidden lg:grid border border-popBlue border-4 col-span-1 lg:col-span-2 lg:mx-20 lg:mt-2'>
+            <div className=' flex justify-center bg-white'>
               <img
               src = {logo.src}
-              className = "object-contain h-44 w-30"
+              className = "lg:object-contain h-22 w-15 lg:h-44 lg:w-30"
               />  
             </div>
       </div>
-      <div className='border col-span-5 row-span-1 mt-2 mr-4'>
+      <div className='border col-span-3 mx-2 lg:col-span-5 row-span-1 lg:mt-2 mt-8 lg:mr-4 '>
           <div className='flex-col text-center bg-white border border-4 border-popBlue rounded p-2'>
-              <div className = "font-bold text-5xl mt-2 text-popBlue">POPULATION COUNCIL</div>
-              <div className = "font-bold text-2xl text-popBlue"> Ideas. Evidence. Impact.</div>
+              <div className = "hidden lg:flex flex-col font-bold text-5xl mt-2 text-popBlue">POPULATION COUNCIL</div>
+              <div className = "hidden lg:flex flex-col font-bold text-2xl text-popBlue"> Ideas. Evidence. Impact.</div>
               <div className = "font-bold text-4xl my-2 text-popBlue"> Disease Tracker</div>
           </div>
           
